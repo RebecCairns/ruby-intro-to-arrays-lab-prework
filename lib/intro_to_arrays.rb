80939077803cccf53b96c7429dfae8b4ad75bfae
Rebecca Cairns <rebecca_cairns@yahoo.com>
2019-03-25 18:29:48 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,19 @@
+def instantiate_new_array
+  newArr = []
+end  
+def array_with_two_elements
+  newArr = ['a', 'b']
+end  
+def first_element(arr)
+  arr[0]
+end
+def third_element(arr)
+  arr[2]
+end  
+def last_element(arr)
+  arr[-1]
+end  
+def first_element_with_array_methods(arr)
+  arr.
+end
+
